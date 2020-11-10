bd037692abd4cbeb339a4ac4780b184ffb77c1df
Isabelly Dias <isabellydias29@gmail.com>
2020-11-10 19:59:19 -0300

    Gerando as entidades do sistema

---
+++ src/main/java/br/com/docket/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.docket.config.audit;
---
+++ src/test/java/br/com/docket/config/NoOpMailConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.docket.config;
+
+import br.com.docket.service.MailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+@Configuration
+public class NoOpMailConfiguration {
+    private final MailService mockMailService;
+
+    public NoOpMailConfiguration() {
+        mockMailService = mock(MailService.class);
+        doNothing().when(mockMailService).sendActivationEmail(any());
+    }
+
+    @Bean
+    public MailService mailService() {
+        return mockMailService;
+    }
+}
